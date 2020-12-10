66433653586ebe2b6662380041b376a5335ceee9
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-10 17:44:10 +0700

    1000 epochs

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,29 +1,31 @@
-import * as tf from '@tensorflow/tfjs'
+import * as tf from "@tensorflow/tfjs";
 
 export async function forecast() {
-  const model = tf.sequential()
+  const model = tf.sequential();
 
-  model.add(tf.layers.dense({
-    inputShape: [1],
-    units: 1
-  }))
+  model.add(
+    tf.layers.dense({
+      inputShape: [1],
+      units: 1,
+    })
+  );
 
   model.compile({
-    optimizer: 'sgd',
-    loss: 'meanSquaredError'
-  })
+    optimizer: "sgd",
+    loss: "meanSquaredError",
+  });
 
-  const xs = tf.tensor1d([1.0, 2.0, 3.0, 4.0, 5.0])
-  const ys = tf.tensor1d([4.0, 7.0, 9.0, 11.0, 13.0])
+  const xs = tf.tensor1d([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+  const ys = tf.tensor1d([3370, 3365, 3387, 3409, 3430, 3707, 3841, 3801]);
 
-  console.log('training...')
+  console.log("training...");
   await model.fit(xs, ys, {
-    epochs: 500
-  })
+    epochs: 1000,
+  });
 
-  const prediction = model.predict(tf.tensor1d([10.0]))
-  let result = prediction.dataSync()
-//   prediction.print()
+  const prediction = model.predict(tf.tensor1d([9.0]));
+  let result = prediction.dataSync();
+  //   prediction.print()
 
-  return result
+  return result;
 }
